285e3885433389d5956aeac149e142595cc1ebec
The Matrix <The Matrix@dasguptapc>
2020-10-03 02:25:34 +0530

    new project

---
+++ Freshvotes1/src/main/java/com/freshvotes1/Freshvotes1Application.java
@@ -0,0 +1,13 @@
+package com.freshvotes1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Freshvotes1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Freshvotes1Application.class, args);
+	}
+
+}
---
+++ Freshvotes1/src/test/java/com/freshvotes1/Freshvotes1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freshvotes1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Freshvotes1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
